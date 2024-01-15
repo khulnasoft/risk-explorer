7007e9267deb3d068f331464c509bc0c4139baed
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2024-01-15 22:24:24 +0000

    Updates

---
+++ precache-manifest.9b5bbb4db3dc5298247efa9958f9525b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f58469d8dea5f3ab07a7159bd690c0a",
+    "url": "/riex/index.html"
+  },
+  {
+    "revision": "ae268aecb4b6bfa02762",
+    "url": "/riex/static/css/main.21e09956.chunk.css"
+  },
+  {
+    "revision": "45a6c5143f4ef143636b",
+    "url": "/riex/static/js/2.2aac0745.chunk.js"
+  },
+  {
+    "revision": "ae268aecb4b6bfa02762",
+    "url": "/riex/static/js/main.112834a5.chunk.js"
+  },
+  {
+    "revision": "ed20327b54de4cffa6d6",
+    "url": "/riex/static/js/runtime~main.88573bde.js"
+  },
+  {
+    "revision": "96c429908275cf93534eec396190a232",
+    "url": "/riex/static/media/AttackTreeExample-modified.96c42990.png"
+  },
+  {
+    "revision": "9632492e9254dc1a4a74525a7892a6f8",
+    "url": "/riex/static/media/KhulnaSoft_R_grad_scrn.9632492e.png"
+  },
+  {
+    "revision": "81b5c2c9335e3ac7817c405f20fceef3",
+    "url": "/riex/static/media/OSS-SuppChainModel-FMC-modified.81b5c2c9.png"
+  },
+  {
+    "revision": "6676fd1246a45ecffa0fd710849c6ea3",
+    "url": "/riex/static/media/assuremoss_logo.6676fd12.png"
+  },
+  {
+    "revision": "acd15c222f9361d2288bd6b1862f0315",
+    "url": "/riex/static/media/attackgraph.acd15c22.svg"
+  },
+  {
+    "revision": "262c8621a2dbe64dbfa1adfbfaded2d0",
+    "url": "/riex/static/media/sparta-logo.262c8621.png"
+  }
+]);
